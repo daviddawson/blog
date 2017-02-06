f7d2485d2d0811be8479673c4315c03231287f5e
David Dawson <david.dawson@simplicityitself.com>
2017-02-06 21:06:57 +0000

    Update analytics

--- buildsite.sh
+++ buildsite.sh
@@ -4,22 +4,22 @@ MYDIR=`pwd`
 
 echo "Starting in $MYDIR"
 
-rm -rf /tmp/muon-doc-gen
+rm -rf /tmp/blog-gen
 
-mkdir /tmp/muon-doc-gen
+mkdir /tmp/blog-gen
 
-git clone -b gh-pages git@github.com:muoncore/documentation.git /tmp/muon-doc-gen
+git clone -b gh-pages git@github.com:daviddawson/blog.git /tmp/blog-gen
 
-cd /tmp/muon-doc-gen
+cd /tmp/blog-gen
 git checkout gh-pages
 cd $MYDIR
 pwd
 
 make render
 
-rsync -av --del  --exclude=".git/" _site/ /tmp/muon-doc-gen
+rsync -av --del  --exclude=".git/" _site/ /tmp/blog-gen
 
-cd /tmp/muon-doc-gen
+cd /tmp/blog-gen
 
 pwd
 git checkout gh-pages
@@ -27,7 +27,7 @@ pwd
 
 git add .
 git status
-git commit -m "Update docs"
+git commit -m "Update blogsite"
 git push origin -f gh-pages
 
 cd $MYDIR
